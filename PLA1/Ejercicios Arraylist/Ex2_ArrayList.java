876bf6d8ddeb490be28fadf2e21633ed310e64cc
acatalaval <55112707+acatalaval@users.noreply.github.com>
2019-09-17 13:07:53 +0200

    Add files via upload

---
+++ PLA1/Ejercicios Arraylist/Ex2_ArrayList.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Ex2_ArrayList {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num_palabras;
+		int long_max=0;
+		String larga="";
+		
+		
+		System.out.println("Escribe una frase: ");
+		String frase = s.nextLine();
+		
+		String[] arrayPalabras = frase.split(" "); //usamos el método split de las arrays para separar la frase en palabras
+		
+		ArrayList<String> alistPalabras= new ArrayList<String>(); // creamos ArrayList
+		
+		for (String palabra : arrayPalabras) { //añadiendo cada palabra al arraylist
+			System.out.println(palabra);       //mostramos el arraylist creado
+			alistPalabras.add(palabra);
+			int long_palabra=palabra.length(); //comparamos longitudes de las palabras para saber cuál es la más larga
+			if (long_palabra > long_max) {
+				long_max = long_palabra;
+				larga=palabra;
+			}
+			
+		}
+		num_palabras=alistPalabras.size(); // nos dará el número de palabras contenidas en la arraylist
+		System.out.println("Hay "+num_palabras+" palabras y la palabra más larga es \'"+larga+"\'");
+	}
+
+}
